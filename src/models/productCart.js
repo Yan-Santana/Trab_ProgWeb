6fb12674924c87b3aa1b2c0c1cfb5458084fc82c
Matheus Gil <matheussoprangil@gmail.com>
2024-05-01 00:19:48 -0400

    feat: new entity and foreign keys added (product, address, order, orderProduct, productCart)

--- src/models/productCart.js
+++ src/models/productCart.js
@@ -1,6 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const { sequelizeClient } = require('./database');
 
+const { Product } = require('./product');
+const { User } = require('./user');
+
 class ProductCart extends Model {}
 
 ProductCart.init(
@@ -21,5 +24,19 @@ ProductCart.init(
     updatedAt: false,
   },
 );
+
+/** MANY TO MANY **/
+User.belongsToMany(Product, {
+  through: ProductCart,
+  as: 'productCart',
+  foreignKey: 'user_id',
+  otherKey: 'product_id',
+});
+Product.belongsToMany(User, {
+  through: ProductCart,
+  as: 'productCart',
+  foreignKey: 'product_id',
+  otherKey: 'user_id',
+});
 
 module.exports = { ProductCart };
