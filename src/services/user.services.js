9b9a4d81f117f3d30c74e63d9c6ebedcf4587a56
FelipeDasr <felipe.heiker07@gmail.com>
2024-05-07 22:03:15 -0400

    feat: new user service method (generate token)

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,6 +1,9 @@
 const { BadRequest } = require('../controllers/utils/errors/badRequest');
 const { userRepository } = require('../repositories/user.repository');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 class UserServices {
   async createUser(data) {
     const userAlreadyExists = await userRepository.findIdByEmail(data.email);
@@ -10,6 +13,23 @@ class UserServices {
     }
 
     return await userRepository.create(data);
+  }
+
+  /**
+   * @param {{
+   *  email: string,
+   *  password: string
+   * }} data
+   */
+  async generateToken(data) {
+    const user = await userRepository.findWithPasswordByEmail(data.email, data.password);
+    if (!user) throw new BadRequest('E-mail não cadastrado');
+
+    const isTheSamePassword = bcrypt.compareSync(data.password, user.password);
+    if (!isTheSamePassword) throw new BadRequest('Senha inválida');
+
+    const token = jwt.sign({ id: user.id }, process.env.APP_SECRET_KEY, { expiresIn: '24h' });
+    return { token };
   }
 }
 
