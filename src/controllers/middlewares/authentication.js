8e003f06f5473faa55e43a526fed949c206b7ec3
Matheus Gil <matheussoprangil@gmail.com>
2024-06-16 20:36:56 -0400

    fix: Token collected properly and error handling

--- src/controllers/middlewares/authentication.js
+++ src/controllers/middlewares/authentication.js
@@ -5,19 +5,24 @@ const { InvalidToken } = require("../utils/errors/InvalidToken");
 
 const jwt = require("jsonwebtoken");
 
-function authorize(req, res, next) {
+async function authorize(req, res, next) {
   try{
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split("Bearer ")[1];
     if (!token) throw new InvalidToken();
 
-    jwt.verify(token, process.env.APP_SECRET_KEY, async (err, decodedToken)=>{
-      if(err) throw new InvalidToken();
+    await new Promise((resolve, reject)=>{
+      jwt.verify(token, process.env.APP_SECRET_KEY, async (err, decodedToken)=>{
+          try{
+            if(err) throw new InvalidToken();
+            const userExists = await userRepository.findById(decodedToken.id);
+            if(!userExists) throw new InvalidToken();
 
-      const userExists = await userRepository.findById(decodedToken.id);
-      if(!userExists) throw new InvalidToken();
-
-      next();
-    })
+            resolve(next());
+          } catch(error){
+            reject(error);
+          }
+        })
+    });
 
   } catch(error) {
     const treatedError = treatError(error);
