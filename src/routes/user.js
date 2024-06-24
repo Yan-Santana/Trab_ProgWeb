0f8bc3bef52779b9a0a0805c917a3582a8c89f82
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-23 21:47:44 -0400

    feat: new app endpoint (/api/users/token-is-valid)

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 const userRouter = express.Router();
 
 const { usersController } = require('../controllers/users.controller');
+const { authorize } = require('../controllers/middlewares/authentication');
 
 userRouter.post('/signup', usersController.signup);
 userRouter.post('/login', usersController.generateToken);
 
-module.exports = { userRouter };
+userRouter.get('/token-is-valid', authorize, usersController.returnThatTokenIsValid);
 
+module.exports = { userRouter };
