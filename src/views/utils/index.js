a96ea8869d800f0ad52743d01095fe4dcdf5a116
Matheus Gil <matheussoprangil@gmail.com>
2024-06-27 22:01:36 -0400

    feat: Made connection between html and javascript for visualizing products

--- src/views/utils/index.js
+++ src/views/utils/index.js
@@ -9,3 +9,29 @@ function getOnlyNumbers(value) {
 function parseNumberToBRL(value) {
   return (value / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
+
+function getStarRating(value) {
+
+  switch(value){
+
+    default:
+    case 0:
+      return "☆☆☆☆☆";
+
+    case 1:
+      return "★☆☆☆☆";
+
+    case 2:
+      return "★★☆☆☆";
+
+    case 3:
+      return "★★★☆☆";
+
+    case 4:
+      return "★★★★☆";
+
+    case 5:
+      return "★★★★★";
+  }
+
+}
