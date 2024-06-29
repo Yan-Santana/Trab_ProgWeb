f66e9662e515a180c9e8db9df34359d2dd2bd08c
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-29 18:43:04 -0400

    feat: new class of error

--- src/controllers/utils/errors/index.js
+++ src/controllers/utils/errors/index.js
@@ -1,9 +1,15 @@
 const { ValidationError } = require('./validationError');
 const { BadRequest } = require('./badRequest');
 const { InvalidToken } = require('./InvalidToken');
+const { NotFound } = require('./notFound');
 
 const treatError = (error) => {
-  if (error instanceof ValidationError || error instanceof BadRequest || error instanceof InvalidToken) {
+  if (
+    error instanceof NotFound ||
+    error instanceof ValidationError ||
+    error instanceof BadRequest ||
+    error instanceof InvalidToken
+  ) {
     return {
       error: error.message,
       code: error.code,
