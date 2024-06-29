5929cf3dcc7251a7ea24367c887647ff13c30068
yan-santana <yansantana63@gmail.com>
2024-06-29 00:03:32 -0400

    refactor: update bug fix

--- src/views/product/script.js
+++ src/views/product/script.js
@@ -1,23 +1,23 @@
-function returnToCatalog(){
-  window.location.href = "../not-found";
+function returnToCatalog() {
+  window.location.href = '../not-found';
 }
 
-function getProductID(){
+function getProductID() {
   const params = new URLSearchParams(window.location.search);
-  const productID = params.get("id");
+  const productID = params.get('id');
 
-  if (!productID){
+  if (!productID) {
     returnToCatalog();
   }
 
   return Number(productID);
 }
 
-async function findAndLoadProduct(){
+async function findAndLoadProduct() {
   const productID = getProductID();
   const product = await (await fetch(`http://localhost:3000/api/products/${productID}`)).json();
 
-  if (!product){
+  if (!product) {
     returnToCatalog();
   }
 
@@ -29,8 +29,8 @@ async function findAndLoadProduct(){
   name.textContent = product.name;
   rating.textContent = getStarRating(product.rating);
   price.textContent = parseNumberToBRL(product.price);
-  image.setAttribute("src", product.photo.url);
-
+  image.setAttribute('src', product.photo.url);
 }
 
-findAndLoadProduct();
+//findAndLoadProduct();
+
